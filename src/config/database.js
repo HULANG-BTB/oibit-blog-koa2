4bc0291aba80f187547c8ee31c6dfc415da5c888
AloneH <admin@oibit.cn>
2021-01-10 13:31:54 +0800

    reactor: database.js

---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+export const database = {
+  host: 'localhost',
+  port: '3306',
+  username: 'root',
+  password: 'root',
+  database: 'oibit-blog',
+  dialect: 'mysql',
+  timezone: '+08:00',
+  pool: {
+    max: 10
+  },
+  additional: {
+    timestamps: true,
+    createdAt: 'create_time',
+    updatedAt: 'update_time',
+    version: 'version'
+  }
+}
+
+export default database
